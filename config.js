16fd7c51e526390168cdec2d180ab2a79c1a7ed6
anxing <anxing131@gmail.com>
2019-10-31 23:30:02 +0800

    - update Graphql service url

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
 
 module.exports = {
     apollo: {
-        public: 'http://localhost:3000/public',
-        auth: 'http://localhost:3000/auth',
-        // public: 'https://wetodos.api.anxing131.xyz/dev/public',
-        // auth: 'https://wetodos.api.anxing131.xyz/dev/auth',
+        // public: 'http://localhost:3000/public',
+        // auth: 'http://localhost:3000/auth',
+        // public: 'https://wetodos.api.anxing131.xyz/dev/dev/public',
+        // auth: 'https://wetodos.api.anxing131.xyz/dev/dev/auth',
+        public: 'https://wetodos.api.anxing131.xyz/prod/prod/public',
+        auth: 'https://wetodos.api.anxing131.xyz/prod/prod/auth',
     }
 }
